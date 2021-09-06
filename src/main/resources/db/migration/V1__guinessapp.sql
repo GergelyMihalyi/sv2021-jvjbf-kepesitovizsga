e1a6a7920c6fa53da04f58db6a6ab4fbd74e0476
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2021-09-06 13:58:03 +0200

    world record database

--- src/main/resources/db/migration/V1__guinessapp.sql
+++ src/main/resources/db/migration/V1__guinessapp.sql
@@ -1,4 +1,8 @@
 create table recorder(id bigint auto_increment, name varchar(60) not null, dob date not null, primary key(id));
+create table world_record(id bigint auto_increment, description varchar(255) not null, value DOUBLE not null,unit_of_measure varchar(255) not null, date_of_record date not null,recorder_id bigint, primary key(id));
+ALTER TABLE world_record
+    ADD CONSTRAINT WORLD_RECORD_RECORDER_ID_FK
+        FOREIGN KEY (recorder_id) REFERENCES recorder(id);
 
 
 
